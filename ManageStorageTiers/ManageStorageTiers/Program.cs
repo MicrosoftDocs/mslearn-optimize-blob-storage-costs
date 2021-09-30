48b9e113a4f4d13d7858e62528d1d59a6855e4b7
David Berry <dberry83702@yahoo.com>
2021-09-30 17:21:59 -0500

    Updated program to use .NET Core 3.1 and new Azure SDK  library

--- ManageStorageTiers/ManageStorageTiers/Program.cs
+++ ManageStorageTiers/ManageStorageTiers/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
 
 namespace ManageStorageTiers
 {
@@ -17,83 +19,64 @@ namespace ManageStorageTiers
 
         static async Task DoWorkAsync()
         {
-            CloudBlobClient cloudBlobClient = null;
-            CloudBlobContainer cloudBlobContainer = null;
+            // The BlobServiceClient object allows you to interact with the storage account
+            string connectionString = Environment.GetEnvironmentVariable("STORAGE_CONNECTION_STRING");
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 
-            // Connect to the user's storage account and create a reference to the blob container holding the sample blobs
-            try
-            {
-                Console.WriteLine("Connecting to blob storage");
-                string storageAccountConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-                string blobContainerName = Environment.GetEnvironmentVariable("CONTAINER_NAME");
-
-                CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
-                cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
-                cloudBlobContainer = cloudBlobClient.GetContainerReference(blobContainerName);
-                Console.WriteLine("Connected");
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine($"Failed to connect to storage account or container: {ex.Message}");
-                return;
-            }
+            // The BlobContainerClient allows us to interact with aspecific container in the storage account
+            string blobContainerName = Environment.GetEnvironmentVariable("CONTAINER_NAME");
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
 
             // Display the details of the blobs, including the storage tier
-            await DisplayBlobTiers(cloudBlobContainer);
+            Console.WriteLine("Initial blob storage tiers");
+            await DisplayBlobTiers(blobContainerClient);
 
-            try
-            {
-                // Change the storage tier for each blob:
-                // Set blob1 to Cool (was Archive)
-                // Note the the effects of this change are not immediate - it can take several hours for a blob to be rehydrated, during which time it remains in the Archive storage tier
-                Console.WriteLine("Moving blob1 to the Cool tier");
-                CloudBlockBlob blob1 = cloudBlobContainer.GetBlockBlobReference("blob1");
-                blob1.SetStandardBlobTier(StandardBlobTier.Cool);
+            Console.WriteLine("Press any key to proceed to updating blob storage tiers");
+            Console.ReadKey();
+            await UpdateBlobTiers(blobContainerClient);
 
-                // Set blob2 to Hot (was Cool)
-                Console.WriteLine("Moving blob2 to the Hot tier");
-                CloudBlockBlob blob2 = cloudBlobContainer.GetBlockBlobReference("blob2");
-                blob2.SetStandardBlobTier(StandardBlobTier.Hot);
-
-                // Set blob3 to Archive (was Hot)
-                Console.WriteLine("Moving blob3 to the Archive tier");
-                CloudBlockBlob blob3 = cloudBlobContainer.GetBlockBlobReference("blob3");
-                blob3.SetStandardBlobTier(StandardBlobTier.Archive);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to change storage tier of blobs: {ex.Message}");
-                return;
-            }
-
-            // Display the details of the blobs again, and confirm that the storage tier for the  Cool and Hot blobs has changed.
-            // The Archive blob (blob1), might still be reported as being in the Archive tier if it has not yet been rehydrated 
-            await DisplayBlobTiers(cloudBlobContainer);
+            Console.WriteLine("Press any key to proceed to display the new blob storage tiers");
+            Console.ReadKey();
+            await DisplayBlobTiers(blobContainerClient);
         }
 
-        private static async Task DisplayBlobTiers(CloudBlobContainer cloudBlobContainer)
+        private static async Task DisplayBlobTiers(BlobContainerClient blobContainerClient)
         {
-            try
+            AsyncPageable<BlobItem> blobItems = blobContainerClient.GetBlobsAsync();
+
+            await foreach (var blobItem in blobItems)
             {
-                // Find the details, including the tier level, for each blob
-                Console.WriteLine("Fetching the details of all blobs");
-                BlobContinuationToken blobContinuationToken = null;
-                do
-                {
-                    var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
-                    blobContinuationToken = results.ContinuationToken;
-                    foreach (CloudBlockBlob item in results.Results)
-                    {
-                        // The StandardBlobTier property indicates the blob tier - Archive, Cool, or Hot
-                        Console.WriteLine($"Blob name {item.Name}: Tier {item.Properties.StandardBlobTier}");
-                    }
-                } while (blobContinuationToken != null);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to retrieve details of blobs: {ex.Message}");
-                return;
+                Console.WriteLine($"  Blob name {blobItem.Name}:   Tier {blobItem.Properties.AccessTier}");
             }
         }
+
+        private static async Task UpdateBlobTiers(BlobContainerClient blobContainerClient)
+        {
+            AsyncPageable<BlobItem> blobItems = blobContainerClient.GetBlobsAsync();
+
+            await foreach (var blobItem in blobItems)
+            {
+                string blobName = blobItem.Name;
+                AccessTier? currentAccessTier = blobItem.Properties.AccessTier;
+                AccessTier newAccessTier = GetNewAccessTier(currentAccessTier);
+
+                Console.WriteLine($"  Blob name: {blobItem.Name}   Current tier: {currentAccessTier}   New tier: {newAccessTier}");
+
+                BlobClient blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
+                blobClient.SetAccessTier(newAccessTier);
+            }
+        }
+
+
+        private static AccessTier GetNewAccessTier(AccessTier? accessTier)
+        {
+            if (accessTier == AccessTier.Hot)
+                return AccessTier.Cool;
+            else if (accessTier == AccessTier.Cool)
+                return AccessTier.Archive;
+            else
+                return AccessTier.Hot;
+        }
+
     }
 }
